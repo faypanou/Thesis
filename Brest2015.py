f139e6aecb5742bbf80d769f3c5163b0336fee8c
faypanou <63549224+faypanou@users.noreply.github.com>
2023-01-15 23:59:03 +0200

    Delete Brest2015.py

--- Brest2015.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv(r'D:/Courses/Διπλωματική/AIS/AIS Breast 2015/ais_brest_locations.csv', header=1)
-shuffled_df = df.sample(frac=1)
-shuffled_df.to_csv('ais_brest_locations_shuffled.csv', index=False)
